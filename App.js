52772a3e8d24e8944d313c4ab3d8611b5cb8cf77
Szagri <szagribk@gmail.com>
2021-12-24 13:13:17 +0100

    Added few images and description to meal

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { StatusBar } from 'expo-status-bar';
 import { Image, TouchableOpacity, FlatList,TouchableHighlight, SectionList, SafeAreaView, StyleSheet, TextInput, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import TextAncestorContext from "react-native-web/dist/exports/Text/TextAncestorContext";
+
+
 
 
 const LoginScreen = ({navigation}) => {
@@ -111,9 +114,9 @@ const JedzenieScreen = ({route, navigation}) => {
     if(itemId == 1) {
         let listViewRef;
                   const [dataSource, setDataSource] = useState([
-                    { id: 1, title: 'Classic'},
-                    { id: 2, title: 'BBQ Burger' },
-                    { id: 3, title: 'Vege Burger' },
+                    { id: 6, title: 'Classic'},
+                    { id: 7, title: 'BBQ Burger' },
+                    { id: 8, title: 'Vege Burger' },
                   ]);
                 const ItemView = ({ item }) => {
                     return (
@@ -158,9 +161,9 @@ const JedzenieScreen = ({route, navigation}) => {
     } else if (itemId == 2) {
     let listViewRef;
                       const [dataSource, setDataSource] = useState([
-                        { id: 1, title: 'Jalapeno Burger'},
-                        { id: 2, title: 'Triple Cheese Burger' },
-                        { id: 3, title: 'Spicy Burger' },
+                        { id: 9, title: 'Jalapeno Burger'},
+                        { id: 10, title: 'Triple Cheese Burger' },
+                        { id: 11, title: 'Spicy Burger' },
                       ]);
                     const ItemView = ({ item }) => {
                         return (
@@ -204,9 +207,9 @@ const JedzenieScreen = ({route, navigation}) => {
     } else if (itemId == 3) {
           let listViewRef;
                             const [dataSource, setDataSource] = useState([
-                              { id: 1, title: 'Drwal'},
-                              { id: 2, title: 'McWrap' },
-                              { id: 3, title: 'McFlurry' },
+                              { id: 12, title: 'Drwal'},
+                              { id: 13, title: 'McWrap' },
+                              { id: 14, title: 'McFlurry' },
                             ]);
                           const ItemView = ({ item }) => {
                               return (
@@ -250,9 +253,9 @@ const JedzenieScreen = ({route, navigation}) => {
    } else if (itemId == 4) {
          let listViewRef;
                            const [dataSource, setDataSource] = useState([
-                             { id: 1, title: 'Kubełek Classic'},
-                             { id: 2, title: 'BeSmart' },
-                             { id: 3, title: 'Frytki' },
+                             { id: 15, title: 'Kubełek Classic'},
+                             { id: 16, title: 'BeSmart' },
+                             { id: 17, title: 'Frytki' },
                            ]);
                          const ItemView = ({ item }) => {
                              return (
@@ -296,9 +299,9 @@ const JedzenieScreen = ({route, navigation}) => {
    } else if (itemId == 5) {
          let listViewRef;
                            const [dataSource, setDataSource] = useState([
-                             { id: 1, title: 'Nuggets'},
-                             { id: 2, title: 'Oreo Shake' },
-                             { id: 3, title: 'Whooper' },
+                             { id: 18, title: 'Nuggets'},
+                             { id: 19, title: 'Oreo Shake' },
+                             { id: 20, title: 'Whooper' },
                            ]);
                          const ItemView = ({ item }) => {
                              return (
@@ -355,12 +358,75 @@ const UstawieniaScreen = ({ navigation}) => {
 
 const PotrawaScreen = ({route, navigation}) => {
     const {itemId} = route.params;
-    return (
-        <View style = {{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-               <Text>Restauracje</Text>
-               <Button title = "Wyloguj" onPress = {() => navigation.navigate("Login")} />
-        </View>
-    );
+    if(itemId === 6) {
+        return (
+            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                <Image
+                    style={styles.logo}
+                    source={require('pam\\assets\\classic.jpg')}
+                />
+                <Text style={styles.nazwa}> Classic Burger </Text>
+                <Text style={styles.opis}> 1x burger wołowy 100% 200g</Text>
+                <Text style={styles.opis}> Papryczka Jalapeno</Text>
+                <Text style={styles.opis}> Bułka świeżo pieczona</Text>
+                <Text style={styles.opis}> Ser od krówki</Text>
+                <Text style={styles.opis}> Cebula</Text>
+                <Text style={styles.opis}> Pomidor</Text>
+            </View>
+        );
+    }
+    if(itemId === 7) {
+        return (
+            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                <Image
+                    style={styles.logo}
+                    source={require('pam\\assets\\bbq.jpg')}
+                />
+                <Text style={styles.nazwa}> BBQ Burger </Text>
+                <Text style={styles.opis}> 1x burger wołowy 100% 200g</Text>
+                <Text style={styles.opis}> Papryczka Jalapeno</Text>
+                <Text style={styles.opis}> Bułka świeżo pieczona</Text>
+                <Text style={styles.opis}> Ser od krówki</Text>
+                <Text style={styles.opis}> Cebula</Text>
+                <Text style={styles.opis}> Pomidor</Text>
+            </View>
+        );
+    }
+    if(itemId === 8) {
+        return (
+            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                <Image
+                    style={styles.logo}
+                    source={require('pam\\assets\\vege.jpg')}
+
+                />
+                <Text style={styles.nazwa}> VEGE Burger </Text>
+                <Text style={styles.opis}> 1x burger wołowy 100% 200g</Text>
+                <Text style={styles.opis}> Papryczka Jalapeno</Text>
+                <Text style={styles.opis}> Bułka świeżo pieczona</Text>
+                <Text style={styles.opis}> Ser od krówki</Text>
+                <Text style={styles.opis}> Cebula</Text>
+                <Text style={styles.opis}> Pomidor</Text>
+            </View>
+        );
+    }
+    if(itemId === 9) {
+        return (
+            <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                <Image
+                    style={styles.logo}
+                    source={require('pam\\assets\\jalapeno.jpg')}
+                />
+                <Text style={styles.nazwa}> Jalapeno Burger </Text>
+                <Text style={styles.opis}> 1x burger wołowy 100% 200g</Text>
+                <Text style={styles.opis}> Papryczka Jalapeno</Text>
+                <Text style={styles.opis}> Bułka świeżo pieczona</Text>
+                <Text style={styles.opis}> Ser od krówki</Text>
+                <Text style={styles.opis}> Cebula</Text>
+                <Text style={styles.opis}> Pomidor</Text>
+            </View>
+        );
+    }
 }
 
 const Stack = createNativeStackNavigator();
@@ -385,15 +451,29 @@ function App() {
 
 const styles = StyleSheet.create({
        itemStyle: {
-          padding: 30,
-          fontSize: 20,
+           padding: 30,
+           fontSize: 20,
         },
       input: {
-        height: 50,
-        margin: 20,
-        borderWidth: 1,
-        padding: 10,
+         height: 50,
+         margin: 20,
+         borderWidth: 1,
+         padding: 10,
       },
+      logo: {
+          flex: 5,
+          height: 70,
+          resizeMode: 'contain',
+      },
+    nazwa: {
+        flex: 1,
+        fontSize: 30,
+        margin: 0,
+        padding: 0,
+    },
+    opis: {
+           flex: 0.5,
+    },
 });
 
 export default App;
